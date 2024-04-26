8b354b558318620ecede6dbd7b45440de76fb889
Barrett Anderson <barrett1614@gmail.com>
2024-04-25 20:58:50 -0500

    Ternary Sign Up option

--- src/components/Login/ProfileEntry.jsx
+++ src/components/Login/ProfileEntry.jsx
@@ -1,29 +1,44 @@
-import { useState } from "react";
-import Input from "./input.jsx";
-import { isEmail, isNotEmpty, hasMinLength } from "./validation.js";
-import useInput from "./useInput.js";
+import { useState, useContext, createContext } from "react";
+const AuthContext = createContext();
 
 ///////////////////////////////////
 /// PUTTING IT ALL TOGETHER //////
 /////////////////////////////////
 
 export default function ProfileEntry() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [register, setRegister] = useState(true);
+  // const { dispatch } = useContext(AuthContext);
+
+  const submitHandler = (e) => {};
+
   return (
     <main>
       <h1 className="login-welcome">Welcome, Pokemon Master!</h1>
-      <form className="form auth-form">
-        <input className="form-input" placeholder="Username" />
-        <input className="form-input" placeholder="Password" />
+      <form className="form auth-form" onSubmit={submitHandler}>
+        <input
+          className="form-input"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="form-input"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <div>
           <input type="checkbox" id="terms" name="terms" required />
           <label className="agree" htmlFor="terms">
             I agree to the terms and conditions
           </label>
         </div>
-        <button className="form-btn">Need to register</button>
+        <button className="form-btn">{register ? "Sign Up" : "Login"}</button>
       </form>
       <div className="container">
-        <button className="form-btn">Log In</button>
+        <button className="form-btn" onClick={() => setRegister(!register)}>
+          Need to {register ? "Login" : "Sign Up"}?
+        </button>
       </div>
     </main>
   );
