c8cbe75c67cbd0fc41e1d00c06d4ea9b1cb7afd3
Barrett Anderson <barrett1614@gmail.com>
2024-04-28 21:42:33 -0500

    Added Notes

--- Server/Server.js
+++ Server/Server.js
@@ -19,15 +19,8 @@ app.post("/login", login);
 // Start the server
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
-// Commit ID of last saved changes:
-// 8b354b558318620ecede6dbd7b45440de76fb889
-
-// Imported Register and Login from Auth,
-// Adjusted Header to include LogInPage route:
-// c407637d64ab27ea8a23cbf00c91ac51a9d83c5d
-
-// Added AuthContext, not used yet:
-// 290110f0cb9a630e330146e2e319a6a591fb692a
+// Stopping place:
+// 4a8296567e5972178129addcbf642e8a866a9eeb
 
 // Use this code to go back:
 // git reset --hard COMMITT_ID
